b583338f751ba558cabaf79f9616031c7e91f10e
OvermindDL1 <overminddl1@gmail.com>
2017-02-09 14:48:32 -0700

    Some internal variable renaming.

--- lib/ex_spirit/detail/tree_map.ex
+++ lib/ex_spirit/detail/tree_map.ex
@@ -4,11 +4,11 @@ defmodule ExSpirit.TreeMap do
 
   def new(), do: %__MODULE__{}
 
-  def add_text(tst, str, value) when is_binary(str), do: add(tst, String.to_charlist(str), value)
-  def add(tst, bin, value) when is_binary(bin), do: add(tst, :erlang.binary_to_list(bin), value)
-  def add(tst, list, value) when is_list(list) do
-    %{tst |
-      root: add_(tst.root, list, value)
+  def add_text(tm, str, value) when is_binary(str), do: add(tm, String.to_charlist(str), value)
+  def add(tm, bin, value) when is_binary(bin), do: add(tm, :erlang.binary_to_list(bin), value)
+  def add(tm, list, value) when is_list(list) do
+    %{tm |
+      root: add_(tm.root, list, value)
     }
   end
 
