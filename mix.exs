2be30b67b649171dfe8497517ec0f1b5e4712649
OvermindDL1 <overminddl1@gmail.com>
2017-06-21 13:51:21 -0600

    Previous tests fixes some of the major leakages, will go through more later, publishing.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExSpirit.Mixfile do
 
   def project do
     [app: :ex_spirit,
-     version: "0.2.6",
+     version: "0.2.7",
      elixir: "~> 1.4",
      description: description(),
      package: package(),
